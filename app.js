1ab2213df6645c6f09992bfa26ec70005f2836b2
nargesrabiee <nargesrabiee11@gmail.com>
2024-07-16 12:45:27 +0330

    Add the ability to complete todos

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { addTodoAction, removeTodoAction, doTodoAction } from "./Redux/actionCreators.js";
 import { addTodo, removeTodo, doTodo } from "./Redux/action.js";
 
+window.completeTodoHandler = completeTodoHandler
+
 // DOM Elem
 const addInput = document.querySelector('.add-input')
 const searchInput = document.querySelector('.search-input')
@@ -9,24 +11,6 @@ const addBtn = document.querySelector('.add-btn')
 const searchBtn = document.querySelector('.search-btn')
 const deleteBtn = document.querySelector('.delete-btn')
 const completeBtn = document.querySelector('.incomplete-btn')
-
-// Functions
-function generateTodoLi(todos) {
-    todosContainerElem.innerHTML = ''
-    todos.forEach(todo => {
-        todosContainerElem.insertAdjacentHTML('beforeend', `
-                <li class="todo-container ${todo.isComplete && 'todo-done'}">
-                    <p class="todo-title">${todo.title}</p>
-                    <div class="todo-btns">
-                        <img class="icon-btn delete-btn" src="./icons/delete.png" alt="delete btn">
-                        <img class="icon-btn incomplete-btn" src="${todo.isComplete ? './icons/complete.png' : './icons/incomplete.png'}" alt="incomplete btn">
-                    </div>
-                    <img class="done-img ${todo.isComplete && 'active'}" src="./icons/done.png" alt="done todo">
-                </li>`
-        )
-
-    })
-}
 
 // Declare Reducer
 function todoReducer(state = [], action) {
@@ -45,6 +29,12 @@ function todoReducer(state = [], action) {
             return state
         }
         case doTodo: {
+            const newState = [...state]
+            newState.some(todo => {
+                if(todo.id === action.id){
+                    todo.isComplete = !todo.isComplete
+                }
+            })
             return state
         }
         default: {
@@ -57,7 +47,7 @@ function todoReducer(state = [], action) {
 const store = Redux.createStore(todoReducer)
 
 // Declare Events
-addBtn.addEventListener('click', (event) => {
+addBtn.addEventListener('click', event => {
     event.preventDefault()
     const newTodoTitle = addInput.value;
     store.dispatch(addTodoAction(newTodoTitle))
@@ -75,3 +65,36 @@ addInput.addEventListener('keypress', event => {
         generateTodoLi(allTodos)
     }
 })
+
+// Functions
+function generateTodoLi(todos) {
+    todosContainerElem.innerHTML = ''
+    todos.forEach(todo => {
+        todosContainerElem.insertAdjacentHTML('beforeend', `
+                <li class="todo-container ${todo.isComplete && 'todo-done'}">
+                    <p class="todo-title">${todo.title}</p>
+                    <div class="todo-btns">
+                        <img 
+                            class="icon-btn delete-btn" 
+                            src="./icons/delete.png" 
+                            alt="delete btn"
+
+                            >
+                        <img 
+                            class="icon-btn incomplete-btn" 
+                            src="${todo.isComplete ? './icons/complete.png' : './icons/incomplete.png'}" 
+                            alt="incomplete btn"
+                            onclick=completeTodoHandler("${todo.id}")
+                        >
+                    </div>
+                    <img class="done-img ${todo.isComplete && 'active'}" src="./icons/done.png" alt="done todo">
+                </li>`
+        )
+    })
+}
+
+function completeTodoHandler(todoID) {
+    store.dispatch(doTodoAction(todoID))
+    const allTodos = store.getState()
+    generateTodoLi(allTodos)  
+}
